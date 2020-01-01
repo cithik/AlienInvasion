08d3a0bf6f4004426e69ea4ccb4ab2960f2b2c0a
cithik <cithik@yahoo.co.in>
2020-01-01 15:55:35 -0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,42 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Ship Settings
+        self.ship_limit = 3
+
+        # Bullet Settings
+        self.bullet_width = 30
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+
+        #alien settings
+        self.fleet_drop_speed = 50
+        self.speedup_scale =3
+        self.score_scale = 3.5
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed = 1.5
+        self.bullet_speed =3.0
+        self.alien_speed =1.0
+
+        self.fleet_direction =1
+
+
+        self.alien_points = 50
+
+    def increase_speed(self):
+        self.ship_speed*=self.speedup_scale
+        self.bullet_speed*=self.speedup_scale
+        self.alien_speed*=self.speedup_scale
+
+        self.alien_points = int(self.alien_points*self.score_scale)
+        //print(self.alien_points)
